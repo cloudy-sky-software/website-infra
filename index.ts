87295faaa0b66e405ec9c75f1cc68684b6f953c6
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-01-16 17:24:57 -0800

    Initial commit to a dedicated repo

---
+++ index.ts
@@ -0,0 +1,51 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as render from "@cloudyskysoftware/pulumi-render";
+
+import { services } from "@cloudyskysoftware/pulumi-render/types/input";
+
+const ownerId = pulumi
+    .output(render.owners.listOwners())
+    .apply(
+        (result) =>
+            result.items.filter((i) => i.owner?.email === "pl@cloudysky.software")[0].owner?.id ||
+            ""
+    );
+
+const staticSiteDetails: services.StaticSiteServiceDetailsArgs = {
+    buildCommand: "make render-build",
+    publishPath: "public",
+    pullRequestPreviewsEnabled: "yes",
+};
+
+const staticSite = new render.services.StaticSite(
+    "website",
+    {
+        name: "Cloudy Sky Software website",
+        ownerId,
+        repo: "https://github.com/cloudy-sky-software/website",
+        autoDeploy: "yes",
+        branch: "main",
+        serviceDetails: staticSiteDetails,
+    },
+    { protect: true }
+);
+
+const envVars = new render.services.EnvVar("envVars", {
+    serviceId: staticSite.id,
+    envVars: [
+        {
+            key: "HUGO_VERSION",
+            value: "0.109.0",
+        },
+    ],
+});
+
+// TODO: Importing child resources is blocked by https://github.com/cloudy-sky-software/pulumi-render/issues/101.
+// const domain = new render.services.CustomDomain(
+//     "wwwCustomDomain",
+//     {
+//         name: "www.cloudysky.software",
+//         serviceId: staticSite.id,
+//     },
+//     { import: "cdm-c9jnkjrru51r6g7orbgg" }
+// );
